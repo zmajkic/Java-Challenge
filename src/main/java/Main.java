712700823b48cf393a09fa9b2b62e24c964338ea
zmajkic <zeljko.majkic@telekom.de>
2021-06-02 09:18:24 +0200

    Part 1 – Java Fundamentals / Java Grundlagen Check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
+		
+		for (int i = 0; i < 8; i++ ) {    
+		    System.out.println( "Geht nicht" );
+		}
+		
+		
+		
+		
 	}
 
 }
