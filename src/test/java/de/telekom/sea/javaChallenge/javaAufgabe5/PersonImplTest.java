b44dd192089834bab1a52274884bdd061fbce7e1
zmajkic <zeljko.majkic@telekom.de>
2021-06-07 07:40:27 +0200

    update

--- src/test/java/de/telekom/sea/javaChallenge/javaAufgabe5/PersonImplTest.java
+++ src/test/java/de/telekom/sea/javaChallenge/javaAufgabe5/PersonImplTest.java
@@ -1,44 +1,40 @@
 package de.telekom.sea.javaChallenge.javaAufgabe5;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 
-
 class PersonImplTest {
 
 	private PersonImpl cut;
-	
-	@BeforeEach 
+
+	@BeforeEach
 	void setup() {
 		cut = new PersonImpl();
 	}
-	
+
 	@Test
 	void test() {
 
-
 		// Arrange
-		Person cut = new PersonImpl("Hans",   "Maier");
-		
+		Person cut = new PersonImpl("Hans", "Maier");
+
 		// Act
-				String vorname = cut.getVorname();	
-		String nachname = cut.getNachname();	
-		
+		String vorname = cut.getVorname();
+		String nachname = cut.getNachname();
 
 		// Assert
 		assertEquals("Hans", vorname);
 		assertEquals("Maier", nachname);
-		
+
 //		fail("Not yet implemented");
 	}
-		
+
 	@AfterEach
 	void teardown() {
 		cut = null;
 	}
-	
-	}
+
+}
